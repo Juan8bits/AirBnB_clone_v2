53927b3d4f911ac34e0ca05ca53897e6b946e3d7
Juan8bits <juan160820@gmail.com>
2021-03-30 01:54:18 +0000

    FileStorage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        else:
+            dict_cls = {}
+            for k, v in FileStorage.__objects.items():
+                cls_storage = v.__class__.__name__
+                if cls == cls_storage:
+                    dict_cls[k] = v
+            return dict_cls
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -25,6 +33,15 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """Delete obj from __objects if it’s inside
+        if obj is equal to None, the method should 
+        not do anything"""
+        if obj is not None and obj in FileStorage.__objects.values():
+            k = obj.__class__.__name__ + '.' + obj.id
+            FileStorage.__objects.pop(k)
+            self.save()
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -36,15 +53,15 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
