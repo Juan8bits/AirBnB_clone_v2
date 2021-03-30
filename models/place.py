de00e0f9549f422dde2caca2f949b10db0859994
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-30 10:56:16 -0500

    Add models

--- models/place.py
+++ models/place.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Colums(Integer, nullable=False, default=0)
+    number_bathrooms = Colums(Integer, nullable=False, default=0)
+    max_guest = Colums(Integer, nullable=False, default=0)
+    price_by_night = Colums(Integer, nullable=False, default=0)
+    latitude = Colums(Float, nullable=False)
+    longitude = Colums(Float, nullable=False)
