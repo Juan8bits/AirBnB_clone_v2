76a8c1cdbd1b6d4d49a8d6c71e803a7afac92caa
Juan8bits <juan160820@gmail.com>
2021-03-31 01:01:08 +0000

    Fix amenity test and remove test from test_model_base

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,9 +2,12 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+from models import amenity
+import pep8
+import unittest
 
 
-class test_Amenity(test_basemodel):
+class Test_Amenity(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
@@ -13,7 +16,26 @@ class test_Amenity(test_basemodel):
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+
+class Testpep8(unittest.TestCase):
+    """Class to do pep8 validation. """
+    def test_pep8(self):
+        """Method to probe pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/amenity.py'
+        file2 = 'tests/test_models/test_amenity.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found errors (or warnings).")
+
+
+class TestDocs_for_file_storage_file(unittest.TestCase):
+    """ Class to check documentation in files."""
+    def test_module_doc(self):
+        """Method to check for module documentation."""
+        self.assertTrue(len(amenity.__doc__) > 0)
+
+    def test_method_docs(self):
+        """Method to check for method´s documentation."""
+        for func in dir(Amenity):
+            self.assertTrue(len(func.__doc__) > 0)
