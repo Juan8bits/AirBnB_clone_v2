2004334d3c8807e9c7a5f02755c2dfb464c26544
Juan8bits <juan160820@gmail.com>
2021-04-15 21:43:32 +0000

    Try some other asnwer

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,10 +14,10 @@ echo "<!DOCTYPE html>
     <title>Test</title>
   </head>
   <body>
-    <p>Nginx html test file</p>
+    <p>html TEST file</p>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
