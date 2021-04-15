b6f1e6b1db18838c8e493300c8e934ead1a8caa6
Juan8bits <juan160820@gmail.com>
2021-04-15 21:31:53 +0000

    Create config file for servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static.
+sudo apt-get -y update
+sudo apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+
+mkdir -p /data/web_static/releases/test/;
+mkdir -p /data/web_static/shared/;
+
+echo "<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='UTF-8'>
+    <title>Test</title>
+  </head>
+  <body>
+    <p>Nginx html test file</p>
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
