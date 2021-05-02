f6e1bf5fe3ea6b4ceb1b4d314c23886f84753d80
Juan8bits <juan160820@gmail.com>
2021-05-02 18:58:34 +0000

    Add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application with two diferents routes. """
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+# Also, we can define stric_slashes for specific redirection inside app.route()
+
+
+@app.route('/')
+def First_page():
+    """ Redirection for '/' route page. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_page():
+    """ Redirection for '/hbnb' page. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    """ Redirection for '/c/ page and return a variable. """
+    text = text.replace("_", " ")
+    return ('C {}'.format(text))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_page(text='is cool'):
+    """ Redirection for /python/ page an return a variable. """
+    return ('Python {}'.format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>')
+def number_page(n):
+    """ Redirection for /number/ page and return a number
+        only if is integer.
+    """
+    return ('{} is a number'.format(n))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
